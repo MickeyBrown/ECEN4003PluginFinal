a0bd3c0b44e0231e4bf663d9384bb9831973e92e
MickeyBrown <58929396+MickeyBrown@users.noreply.github.com>
2021-12-15 14:55:03 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import source
 
 app = Flask(__name__)
@@ -14,9 +14,30 @@ def home():
 def form_add(): #data has to be sent to the add plugin function
     return render_template('addPlugin.html')
 
+@app.route("/add_submit/", methods = ['POST', 'GET'])
+def data_add():
+	if request.method == 'GET':
+		return render_template("home.html")
+		
+	if request.method == 'POST':
+		form_data = request.form
+		source.addPlugin(form_data['Name'], form_data['Type'], form_data['Port'], form_data['Archive'], form_data['Language'], form_data['Test'], form_data['Description'], form_data['Endpoint'], form_data['Repository'], form_data['DockerFiler'], form_data['URL'])
+		return render_template('add_submit.html', plugin_name = form_data['Name'])
+
 @app.route("/removePlugin")
 def form_remove(): #data has to be sent to the remove plugin function
     return render_template('removePlugin.html')
+
+@app.route("/remove_submit/", methods = ['POST', 'GET'])
+def data_remove():
+	if request.method == 'GET':
+		return render_template("home.html")
+	if request.method == 'POST':
+		form_data = request.form
+		if source.removePlugin(form_data['Name']):
+			return render_template("remove_submit.html", plugin_name = form_data['Name'])
+		else:
+			return render_template('remove_none.html')
 
 @app.route("/urlStatus")
 def urlStatus():
